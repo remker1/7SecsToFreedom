0157aa297963e52f9a9b09655e57c067d957d11c
arehman36 <dpu196@usask.ca>
2024-11-07 17:35:24 -0600

    made changes'

--- level-1/Scripts/fish_bowl.gd
+++ level-1/Scripts/fish_bowl.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -400.0
 @onready var animation = $AnimatedSprite2D
 @onready var sfx_jump = $sfx_jump
 @onready var sfx_waterswoosh: AudioStreamPlayer2D = $sfx_waterswoosh
+@onready var death_noise: AudioStreamPlayer2D = $death_noise
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +15,11 @@ func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+		if velocity.y >= 600: 
+			print("play death")	# if we hit the floor too hard, we die
+			death_noise.play(0.05) # death noise
+			#get_tree().reload_current_scene() # restarting.....
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -34,13 +40,14 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 	
-
+# currently not working as expected. Should playu a sound if we hit a wall but only once, it does it many times.
 	if is_on_wall():		
 			if not sfx_waterswoosh.playing:				
 					sfx_waterswoosh.play(0.50)
 	else:
 			sfx_waterswoosh.stop()
 
+	
 	
 	
 	
