290331c37d313b1d25ac2acf5cef1bf3773e5753
remker1 <liusihan64h@outlook.com>
2024-11-29 16:12:04 -0600

    Main menu is made, Pulse Menu is made, Game Over Menu is made. Next step, make Level selection Menu.

--- level-1/Scripts/panel.gd
+++ level-1/Scripts/panel.gd
@@ -1,27 +1,33 @@
 extends Panel
 
-var time: float = 0.0
+var time: float = 7.0  # Start the countdown from 7 seconds
 var minutes: int = 0
 var seconds: int = 0
 var msec: int = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	time += delta
-	msec = fmod(time, 1) * 100
-	seconds = fmod(time, 60)
-	minutes = fmod(time, 3600) / 60
+	time -= delta  # Decrement the time
+	if time < 0:
+		time = 0  # Ensure it doesn't go below 0
+
+# Calculate minutes, seconds, and milliseconds from time
+	msec = int(fmod(time, 1) * 1000)
+	seconds = int(fmod(time, 60))
+	minutes = int(time / 60)
+
+# Update text labels with formatted time
 	$Minutes.text = "%02d:" % minutes
 	$Seconds.text = "%02d." % seconds
 	$Msecs.text = "%03d" % msec
-	
-	if time >= 7.0:
+
+# Stop the countdown when time reaches 0
+	if time <= 0:
 		stop()
-	
+
 func stop() -> void:
-	set_process(false)	 
-	get_tree().reload_current_scene() # reloading current scene if we exceed 7 seconds
-	
-	
-func get_time_formateed() -> String:
+	set_process(false)
+	# Perform any action when the countdown ends
+	get_tree().reload_current_scene()  # Example action: Reload current scene
+
+func get_time_formatted() -> String:
 	return "%02d:%02d.%03d" % [minutes, seconds, msec]
--- level-1/Scripts/ui_manager.gd
+++ level-1/Scripts/ui_manager.gd
@@ -1,11 +1,10 @@
 extends CanvasLayer
 
-const GAME_SCENE = preload("res://Scenes/game_scene.tscn")
+const GAME_SCENE = preload("res://Scenes/level_0_game_scene.tscn")
 var game_scene : Node2D
 @onready var main_menu: Control = $MainMenu
 @onready var pulse_menu: Control = $PulseMenu
 @onready var game_over_menu: Control = $GameOverMenu
-@onready var time: Label = $GameOverMenu/Panel/Time
 
 var starttime
 
@@ -19,10 +18,12 @@ func _ready() -> void:
 func _on_start_pressed() -> void:
 	game_scene = GAME_SCENE.instantiate()
 	get_parent().add_child(game_scene)
-	game_scene.game_over.connect(_on_game_scene_game_over)
+	#game_scene.game_over.connect(_on_game_scene_game_over)
 	main_menu.hide()
 	get_tree().paused = false
-	starttime = Time.get_unix_time_from_system()
+
+func _on_levels_pressed() -> void:
+	pass # Levels Screen
 
 
 func _on_exit_pressed() -> void:
@@ -44,7 +45,7 @@ func _on_pulse_exit_pressed() -> void:
 	get_tree().quit()
 	
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ESC") && !main_menu.visible :
+	if Input.is_action_just_pressed("Quit") && !main_menu.visible :
 		pulse_menu.visible = !pulse_menu.visible
 	if main_menu.visible || pulse_menu.visible || game_over_menu.visible:
 		get_tree().paused = true
@@ -63,9 +64,5 @@ func _on_over_exit_pressed() -> void:
 
 func _on_game_scene_game_over() -> void:
 	game_over_menu.show()
-	var endtime = Time.get_unix_time_from_system()
-	var timetaken = endtime - starttime
-	var datetime = Time.get_datetime_dict_from_unix_time(timetaken)
-	time.text = "Time Taken: "+str(datetime["hour"])+" : "+str(datetime["minute"])+" : " + str(datetime["second"])
 	game_scene.queue_free()
 	
