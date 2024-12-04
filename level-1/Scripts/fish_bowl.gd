30e41d0454831e7a98aea7f1d5f422086612c61a
Abdul Ismaeel (dpu196) <dpu196@mail.usask.ca>
2024-12-04 14:29:28 -0600

    Revert "Merge branch 'ARBRANCH' into 'main'"
    This reverts merge request !29

--- level-1/Scripts/fish_bowl.gd
+++ level-1/Scripts/fish_bowl.gd
@@ -1,30 +1,24 @@
 extends CharacterBody2D
 
-
+@onready var collide_2: TileMapLayer = $"../Collide-2"
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @onready var animation = $AnimatedSprite2D
 @onready var sfx_jump = $sfx_jump
 @onready var sfx_waterswoosh: AudioStreamPlayer2D = $sfx_waterswoosh
-@onready var death_noise: AudioStreamPlayer2D = $death_noise
 
 
-func _handle_fall_death():
-	await get_tree().create_timer(0.5).timeout  # Wait for the sound to finish
-	get_tree().reload_current_scene()  # Restart scene
-
-	
 func _physics_process(delta: float) -> void:
-		
+	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-		if velocity.y >= 600: 
-			animation.hide()
-			print("play death")  # If we hit the floor too hard, we die
-			death_noise.play(0.05)  # Play death noise
-			call_deferred("_handle_fall_death")  # Defer handling the death to avoid physics interruptions
+		if velocity.y >= 600:
+			# Death animation still in progress, Console Message as replacement.
+			print("Die Animation")
+			get_tree().reload_current_scene()
+			
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -63,7 +57,6 @@ func _physics_process(delta: float) -> void:
 	else:
 			sfx_waterswoosh.stop()
 
-	
 	
 	
 	
